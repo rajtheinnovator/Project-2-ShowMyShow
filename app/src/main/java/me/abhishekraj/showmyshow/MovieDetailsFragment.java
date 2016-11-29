31588e4f26dbc8d3f675c7f9501c32cfa729f410
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2016-11-30 00:17:52 +0530

    Removed ArrayAdapter and ListView(commented out code) from MovieDetailsActivity

--- app/src/main/java/me/abhishekraj/showmyshow/MovieDetailsFragment.java
+++ app/src/main/java/me/abhishekraj/showmyshow/MovieDetailsFragment.java
@@ -50,22 +50,6 @@ public class MovieDetailsFragment extends Fragment {
                     .load(url)
                     .placeholder(R.mipmap.ic_launcher)
                     .into(imageViewBackdrop);
-
-//            ArrayList<Movie> movieArrayList = new ArrayList<>();
-//            movieArrayList.add(movie);
-//
-//            // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-//            // adapter knows how to create list items for each item in the list.
-//            MovieDetailsAdapter adapter = new MovieDetailsAdapter(getActivity(), movieArrayList);
-//
-//            // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-//            // There should be a {@link ListView} with the view ID called list, which is declared in the
-//            // word_list.xml layout file.
-//            ListView listView = (ListView) rootView.findViewById(R.id.list_item);
-//
-//            // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-//            // {@link ListView} will display list items for each {@link Word} in the list.
-//            listView.setAdapter(adapter);
         }
 
         return rootView;
